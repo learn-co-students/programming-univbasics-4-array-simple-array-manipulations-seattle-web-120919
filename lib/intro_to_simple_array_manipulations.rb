7aae5819ed7a17ae215eca754c9e41d727a01d0a
Jazz Bozner <jazzbozner@gmail.com>
2020-01-07 00:17:51 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,47 +1,52 @@
-def using_push(array, string)
-  array.push(string)
+require 'pry'
+
+def using_push(arr, element)
+  arr.push(element)
 end
 
-def using_unshift(array, string)
-  array.unshift(string)
+def using_unshift(arr, element)
+  arr.unshift(element)
 end
 
-def using_pop(array)
-  array.pop
+def using_pop(arr)
+  arr.pop
 end
 
-def pop_with_args(array)
-  array.pop(2)
+def pop_with_args(arr)
+  arr.pop(2)
 end
 
-def using_shift(array)
-  array.shift
+def using_shift(arr)
+  arr.shift
 end
 
-def shift_with_args(array)
-  array.shift(2)
+def shift_with_args(arr)
+  arr.shift(2)
 end
 
-def using_concat(array_one, array_two)
-  array_one.concat(array_two)
+def using_concat(arr, arr1)
+  arr.concat(arr1)
 end
 
-def using_insert(array, element)
-  array.insert(4, element)
+def using_insert(arr, element)
+  arr.insert(4, element)
 end
 
-def using_uniq(array)
-  array.uniq
+def using_uniq(arr)
+  arr.uniq
 end
 
-def using_flatten(array)
-  array.flatten
+def using_flatten(arr)
+  arr.flatten
 end
 
-def using_delete(array, string)
-  array.delete(string)
+def using_delete(arr, element)
+  arr.delete(element)
 end
 
-def using_delete_at(array, integer)
-  array.delete_at(integer)
+def using_delete_at(arr, element)
+  arr.delete_at(element)
 end
+
+
+
